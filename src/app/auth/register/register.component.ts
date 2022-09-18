3f8e9bbd077b8ae5b6b8d454ea4c8041cb9595be
Sofibe <anasofiabepo@gmail.com>
2022-09-18 15:18:05 -0400

    PAGE OF REGISTER

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,4 +1,6 @@
+import { ThisReceiver } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,10 +9,28 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class RegisterComponent implements OnInit {
+  registerForm!: FormGroup;
 
-  constructor() { }
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.registerForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      email: ['',[ Validators.required, Validators.email]],
+      password: new FormControl(null, [
+        (c: AbstractControl) => Validators.required(c),
+        Validators.pattern(
+          /(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@$!%*#?&^_-]).{8,}/
+        ),
+      ])
+    })
+  }
+
+  createUser(){
+    console.log(this.registerForm.valid);
+    console.log(this.registerForm.value);
+    console.log(this.registerForm.errors);
   }
 
 }
